05089df32e282ac3e91bf6ebf88a1d3e2b127602
Gabriel <66729017+Gabriel-antoni@users.noreply.github.com>
2021-09-06 15:48:21 -0300

    adiciona conatos

---
+++ src/Components/footer/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+import "./footer.css"
+
+export default class Footer extends Component {
+  render() {
+    return (
+    <div id="footer">
+      <footer className="descricao">
+     <h5>Desenvolvido por Gabriel 2021</h5>
+     </footer>
+    </div>
+    );
+  }
+  }
--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -1,29 +1,170 @@
 import React, { Component } from "react";
 import Menu from "../Header/header";
-import Modelo1 from '../../assents/imagem/modelo1.jpg';
+import Modelo from "../../assents/imagem/modelo1.jpg";
+import Gmail from "../../assents/imagem/Gmail.png";
+import Facebook from "../../assents/imagem/facebook.png";
+import Twitter from "../../assents/imagem/twitter.png";
+import Whatsapp from "../../assents/imagem/whatsapp.png";
+import Instagram from "../../assents/imagem/instagram.png"
+import Github from "../../assents/imagem/github.png";
+import Footer from "../footer/index";
 
-import "./home.css"
+import "./home.css";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container">
         <Menu />
-        <div className="feed">
+        <main className="feed">
           <div className="caixa1">
-           
+          <div class="image">
+            <img src= {Modelo} />
+          </div>
+
+          <div className="titulo">
+            <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Ea deleniti aperiam nisi quaerat ex, quos tenetur consequatur</p>
+          </div>
+
           </div>
           <div className="caixa2">
-            
+          <div class="image">
+          <img src= {Modelo} />
+          </div>
+
+          <div className="titulo">
+            <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Ea deleniti aperiam nisi quaerat ex, quos tenetur consequatur</p>
+          </div>
+
             </div>
           <div className="caixa3">
-        
+          <div class="image">
+          <img src= {Modelo} />
+          </div>
+
+          <div className="titulo">
+            <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Ea deleniti aperiam nisi quaerat ex, quos tenetur consequatur</p>
+          </div>
+          </div>
+
+          <div className="caixa4">
+          <div class="image">
+          <img src= {Modelo} />
+          </div>
+
+          <div className="titulo">
+            <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Ea deleniti aperiam nisi quaerat ex, quos tenetur consequatur</p>
+          </div>
+
+          </div>
+
+          <div className="caixa5">
+          <div class="image">
+          <img src= {Modelo} />
+          </div>
+
+          <div className="titulo">
+            <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Ea deleniti aperiam nisi quaerat ex, quos tenetur consequatur</p>
+          </div>
+
+          </div>
+
+          <div className="caixa6">
+          <div class="image">
+          <img src= {Modelo} />
+          </div>
+
+          <div className="titulo">
+            <p className="sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Ea deleniti aperiam nisi quaerat ex, quos tenetur consequatur</p>
+          </div>
+
           </div>
           
-        </div>
+        </main>
         <div className="contatos">
-          
+          <div className="sobre">
+          <div className="p1">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Quia deleniti rerum aut, eum aperiam quasi ipsum deserunt
+               incidunt, obcaecati earum ex, similique quas veniam laboriosam 
+               necessitatibus? Accusantium totam quisquam porro!</p>
+            </div>
+            <div className="p2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Quia deleniti rerum aut, eum aperiam quasi ipsum deserunt
+               incidunt, obcaecati earum ex, similique quas veniam laboriosam 
+               necessitatibus? Accusantium totam quisquam porro!</p>
+            </div>
+            <div className="p3">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Quia deleniti rerum aut, eum aperiam quasi ipsum deserunt
+               incidunt, obcaecati earum ex, similique quas veniam laboriosam 
+               necessitatibus? Accusantium totam quisquam porro!</p>
+            </div>
+            <div className="p4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Quia deleniti rerum aut, eum aperiam quasi ipsum deserunt
+               incidunt, obcaecati earum ex, similique quas veniam laboriosam 
+               necessitatibus? Accusantium totam quisquam porro!</p>
+            </div>
+          </div>
+          <div className="redes">
+            <ul id="instagram">
+              <li>
+                <a className="link" href = "#">
+                  <img src={Instagram}></img></a>
+              </li>
+              <li className="lista">Insgram</li>
+            </ul>
+            <ul id="wpp">
+            <li>
+              <a className="link" href = "#">
+                  <img src={Whatsapp}></img>
+                </a>
+              </li>
+              <li className="lista">whatsapp</li>
+            </ul>
+
+            <ul id="tw">
+            <li>
+              <a className="link" href = "#">
+                  <img src={Twitter}></img>
+                </a>
+              </li>
+            <li className="lista">Twitter</li>
+            </ul>
+            <ul id="git">
+            <li>
+              <a className="link" href = "#">
+                  <img src={Github}></img>
+                </a>
+              </li>
+            <li className="lista">Github</li>
+            </ul>
+            <ul id="face">
+            <li>
+              <a className="link" href = "#">
+                  <img src={Facebook}></img></a>
+              </li>
+            <li className="lista">Facebook</li>
+            </ul>
+            <ul id="gmail">
+            <li>
+              <a className="link" href = "#">
+                  <img src={Gmail}></img>
+                </a>
+              </li>
+            <li className="lista">Gmail</li>
+            </ul>
+          </div>
         </div>
+        <Footer />
       </div>
     );
   }
